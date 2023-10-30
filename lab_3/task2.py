e58e9e1101ed4ef0c1093d7cd3d39c68c09309c5
Paul Kalabin <114708047+zafod42@users.noreply.github.com>
2023-10-30 09:51:56 +0300

    Лабораторная 3

---
+++ lab_3/task2.py
@@ -0,0 +1,18 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(first_group, second_group, separator=","):
+    participants_first = first_group.split(separator)
+    participants_second = second_group.split(separator)
+
+    # Конвертация в множество для последующего применения intersection
+    unique_part_first = set(participants_first)
+    unique_part_second = set(participants_second)
+
+    common_participants = list(set.intersection(unique_part_second, unique_part_first))
+    common_participants.sort()
+    return common_participants
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+print(find_common_participants(participants_first_group,participants_second_group,"|"))
